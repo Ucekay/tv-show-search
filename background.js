e641f469a40ad47947ab5e2c3dbdbe4049a3d189
Ucekay <u331951c@ecs.osaka-u.ac.jp>
2024-03-28 13:42:04 +0900

    context menu search

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener((details) => {
+  chrome.contextMenus.create({
+    title: "Test Context Menu",
+    id: "contextMenu1",
+    contexts: ["page", "selection"],
+  });
+  chrome.contextMenus.onClicked.addListener((event) => {
+    console.log(event);
+    chrome.tabs.create({ url: `https://www.imdb.com/find/?q=${event.selectionText}&ref_=nv_sr_sm` });
+  });
+});
+
+console.log("background script is running...");
